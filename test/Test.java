0497f81fa71673d38048d04d4b3e3974559da177
MaxOmenes <ukhin.maxim@gmail.com>
2023-10-21 00:20:28 +0300

    Test Interpolation Added

--- test/Test.java
+++ test/Test.java
@@ -1,15 +1,18 @@
+import com.cgvsu.interpolation.RadialInterpolation;
 import com.cgvsu.models.Point;
 import com.cgvsu.models.Sector;
+import javafx.scene.paint.Color;
 
 import java.util.List;
 
 public class Test {
     @org.junit.jupiter.api.Test
-    public void testSector(){
-        List<Point> c = Sector.getSector(300, 300, 100, 600, 500, 500, 200);
-        for(Point c1:c){
-            System.out.println(c1.x+", "+ c1.y);
+    public void TestInterpolation(){
+        RadialInterpolation ri = new RadialInterpolation(100, 100, 2, Color.AQUA, Color.CRIMSON);
+        if (!ri.getInterpolationPixel(102, 102).getC0().equals(Color.CRIMSON)){
+            throw new RuntimeException();
         }
+
 
 
     }
